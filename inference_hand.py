fix rotation training bugs

inference_hand.py
@@ -107,7 +107,7 @@ def visualize_joints_in_rgb(Trajs:List[HandDenoiseTraj],
         vertices_list.append(vertices)
     intrinsics = intrinsics.cpu().numpy()
     border_color = [255, 0, 0]
-    _, height, width, _ = rgb_frames.shape
+    video_len, height, width, _ = rgb_frames.shape
     if resize is not None:
         width, height = resize
     else:
@@ -189,14 +189,11 @@ class Args:
     """Whether to save the output trajectory, which will be placed under `traj_dir/egoallo_outputs/some_name.npz`."""
     visualize_traj: bool = False
     """Whether to visualize the trajectory after sampling."""
-    using_mat: bool = False
-    """whether to use rotation matrix as gt"""
 
 def inference_and_visualize(
         denoiser_network,
         train_batch,
         device,
-        using_mat=False,
         visualized=True):
     noise_constants = CosineNoiseScheduleConstants.compute(timesteps=1000).to(
         device=device,
@@ -206,6 +203,7 @@ def inference_and_visualize(
     batch,seq_len,_ = train_batch.mano_pose.shape
     train_batch.to(device)
     print(denoiser_network)
+    using_mat = denoiser_network.config.using_mat
     x_0_packed = hand_network.HandDenoiseTraj(
         mano_betas=train_batch.mano_betas.unsqueeze(1).expand((batch, seq_len, 10)),
         mano_poses=train_batch.mano_pose[:,:,3:48].reshape(batch,seq_len,15,3),
@@ -265,7 +263,7 @@ def inference_and_visualize(
                                                     global_translation=x_0_packed.global_translation[:, start_t:end_t, :],
                                                     mano_side=x_0_packed.mano_side,
                                                     ).to(device)
-                x_0_packed_pred[:, start_t:end_t, :] = (
+                x_0_packed_pred[:, start_t:end_t, :] += (
                     denoiser_network.forward(
                         x_t_packed[:, start_t:end_t, :],
                         torch.tensor([t], device=device).expand((batch,)),
@@ -273,7 +271,7 @@ def inference_and_visualize(
                         project_output_rotmats=False,
                         conds=conds,
                         mask=None,
-                    )
+                    )* overlap_weights_slice
                 )
 
             # Take the mean for overlapping regions.
@@ -348,10 +346,9 @@ def inference_and_visualize(
 
 def main(args: Args) -> None:
     device = torch.device("cuda")
-    dataset = HandHdf5Dataset(split="test",dataset_name = 'dexycb')
+    dataset = HandHdf5Dataset(split="test",dataset_name = 'dexycb',vis=True)
     print("Dataset size:", len(dataset))
     visualized = args.visualize
-    using_mat = args.using_mat
     test_hamer = args.Test_hamer
     if test_hamer == True:
         N = len(dataset)
@@ -451,7 +448,7 @@ def main(args: Args) -> None:
                 train_batch.append(sample)
             keys = vars(train_batch[0]).keys()
             train_batch = type(train_batch[0])(**{k: torch.stack([getattr(b, k) for b in train_batch]) for k in keys})
-            pred = inference_and_visualize(denoiser_network,train_batch,device,using_mat,visualized)
+            pred = inference_and_visualize(denoiser_network,train_batch,device,visualized)
             _,_,joints_pred = pred.apply_to_hand()
             error_joints =  torch.sqrt(((train_batch.mano_joint_3d.to(device) - joints_pred)**2).sum(dim=-1)).mean(dim=-1).sum().cpu().numpy()
             print("Joint error is ", error_joints)
@@ -476,7 +473,7 @@ def main(args: Args) -> None:
                                                     drop_last=False)
             for train_batch in dataloader:
                 total_size += train_batch.mano_betas.shape[0]
-                pred = inference_and_visualize(denoiser_network,train_batch,device,using_mat,visualized)
+                pred = inference_and_visualize(denoiser_network,train_batch,device,visualized)
                 batch,seq_len,_ = train_batch.mano_pose.shape
                 loss_mask = train_batch.mask
                 x_0_packed = hand_network.HandDenoiseTraj(

src/egoallo/hand_network.py
@@ -50,27 +50,14 @@ class HandDenoiseTraj(TensorDataclass):
     mano_side: Float[Tensor, "*#batch timesteps 1"]
     """right or left hand"""
     def __init__(self,**kwargs):
-        if "using_mat" not in kwargs.keys():
-            using_mat = False
-        else:
-            using_mat = kwargs["using_mat"]
-        if using_mat:
-            self.mano_betas=kwargs["mano_betas"]
-            self.mano_poses=kwargs["mano_poses"]
-            self.mano_poses_mat=kwargs["mano_poses_mat"]
-            self.global_orientation=kwargs["global_orientation"]
-            self.global_ori_mat=kwargs["global_ori_mat"]
-            self.global_translation=kwargs["global_translation"]
-            self.mano_side=kwargs["mano_side"]
-        else:
-            *batch,time,_,_=kwargs["mano_poses"].shape
-            self.mano_betas=kwargs["mano_betas"]
-            self.mano_poses=kwargs["mano_poses"]
-            self.global_orientation=kwargs["global_orientation"]
-            self.global_translation=kwargs["global_translation"]
-            self.mano_side=kwargs["mano_side"]
-            self.mano_poses_mat=SO3.exp(kwargs["mano_poses"]).as_matrix().reshape((*batch,time,15,9))
-            self.global_ori_mat=SO3.exp(kwargs["global_orientation"]).as_matrix().reshape((*batch,time,9))
+        *batch,time,_,_=kwargs["mano_poses"].shape
+        self.mano_betas=kwargs["mano_betas"]
+        self.mano_poses=kwargs["mano_poses"]
+        self.global_orientation=kwargs["global_orientation"]
+        self.global_translation=kwargs["global_translation"]
+        self.mano_side=kwargs["mano_side"]
+        self.mano_poses_mat=SO3.exp(kwargs["mano_poses"]).as_matrix().reshape((*batch,time,15,9))
+        self.global_ori_mat=SO3.exp(kwargs["global_orientation"]).as_matrix().reshape((*batch,time,9))
     @staticmethod
     def get_packed_dim(using_mat:bool) -> int:
         if using_mat:
@@ -130,19 +117,24 @@ class HandDenoiseTraj(TensorDataclass):
         assert num_joints == 15
         value_list = vars(self)
         if using_mat==True:
-            value_list.pop("mano_poses")
-            value_list.pop("global_orientation")
-        else:
-            value_list.pop("mano_poses_mat")
-            value_list.pop("global_ori_mat")
-        return torch.cat(
+            return torch.cat(
             [
-                x.reshape((*batch, time, -1))
-                for x in value_list.values()
-                if x is not None
+                value_list[x].reshape((*batch, time, -1))
+                for x in value_list.keys()
+                if x not in ["mano_poses","global_orientation"]
             ],
             dim=-1,
-        )
+            )
+        else:
+            return torch.cat(
+            [
+                value_list[x].reshape((*batch, time, -1))
+                for x in value_list.keys()
+                if x not in ["mano_poses_mat","global_ori_mat"]
+            ],
+            dim=-1,
+            )
+        
 
     @classmethod
     def unpack(
@@ -163,9 +155,9 @@ class HandDenoiseTraj(TensorDataclass):
             mano_betas, mano_poses_mat, global_ori_mat,global_translation,mano_side = torch.split(
                 x, [10, 15 * 9, 9, 3, 1], dim=-1
             )
-            mano_poses_mat = mano_poses_mat.reshape((*batch, time, 15, 3, 3))
-            mano_poses = SO3.from_matrix(mano_poses_mat).log()
-            global_orientation = SO3.from_matrix(global_orientation).log()
+            mano_poses_mat = mano_poses_mat.reshape((*batch, time, 15, 9))
+            mano_poses = SO3.from_matrix(mano_poses_mat.reshape((*batch, time, 15, 3, 3))).log()
+            global_orientation = SO3.from_matrix(global_ori_mat.reshape(((*batch, time, 3, 3)))).log()
         else:
             mano_betas, mano_poses, global_orientation,global_translation,mano_side = torch.split(
                 x, [10, 15 * 3, 3, 3, 1], dim=-1
@@ -181,12 +173,9 @@ class HandDenoiseTraj(TensorDataclass):
         #     hand = project_rotmats_via_svd(body_rotmats)
 
         return HandDenoiseTraj(
-            using_mat=True,
             mano_betas=mano_betas,
             mano_poses=mano_poses,
-            mano_poses_mat=mano_poses_mat,
             global_orientation=global_orientation,
-            global_ori_mat=global_ori_mat,
             global_translation=global_translation,
             mano_side=mano_side
         )
@@ -437,13 +426,22 @@ class HandDenoiser(nn.Module):
         assert num_hand_joints == 15
 
         # Encode the trajectory into a single vector per timestep.
-        x_t_encoded = (
-            self.encoders["mano_betas"](x_t.mano_betas.reshape((batch, time, -1)))
-            + self.encoders["mano_poses"](x_t.mano_poses.reshape((batch, time, -1)))
-            + self.encoders["global_orientation"](x_t.global_orientation)
-            + self.encoders["global_translation"](x_t.global_translation)
-            + self.encoders["mano_side"](x_t.mano_side)
-        )
+        if config.using_mat:
+            x_t_encoded = (
+                self.encoders["mano_betas"](x_t.mano_betas.reshape((batch, time, -1)))
+                + self.encoders["mano_poses_mat"](x_t.mano_poses_mat.reshape((batch, time, -1)))
+                + self.encoders["global_ori_mat"](x_t.global_ori_mat)
+                + self.encoders["global_translation"](x_t.global_translation)
+                + self.encoders["mano_side"](x_t.mano_side)
+            )
+        else:
+            x_t_encoded = (
+                self.encoders["mano_betas"](x_t.mano_betas.reshape((batch, time, -1)))
+                + self.encoders["mano_poses"](x_t.mano_poses.reshape((batch, time, -1)))
+                + self.encoders["global_orientation"](x_t.global_orientation)
+                + self.encoders["global_translation"](x_t.global_translation)
+                + self.encoders["mano_side"](x_t.mano_side)
+            )
         assert x_t_encoded.shape == (batch, time, config.d_latent)
 
         # Embed the diffusion noise level.