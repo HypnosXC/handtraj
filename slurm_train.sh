35413ea0fb96e58090cd3ece5e2eac18b83e7fb1
HypnosXC <q2455688@163.com>
2026-03-21 06:02:18 +0800

    add more training configs

--- slurm_train.sh
+++ slurm_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --gres=gpu:8
 #SBATCH --mem=0
@@ -14,4 +14,4 @@
 source activate handtraj
 
 # ---- 在这里写你的命令 ----
-accelerate launch --num_processes 8 --num_machines 1 train_hand_motion_prior.py --config.experiment-name image_visual_sublen_128 --config.batch_size 256 --config.subseq_len 128
+accelerate launch --num_processes 8 --num_machines 1 train_hand_motion_prior.py --config configs/hand_motion_prior_flow_matching.yaml
